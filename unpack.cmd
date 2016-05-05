02ea897b6be384b2b3d8ba73394661b598e53f67
myfreeer <myfreeer@gmail.com>
2016-05-05 18:13:33 +0800

    Update unpack.cmd

--- unpack.cmd
+++ unpack.cmd
@@ -6,5 +6,5 @@ set file=chrome_100_percent.pak
 if not exist "%WORK_DIR%" md "%WORK_DIR%"
 pushd "%WORK_DIR%"
 if not exist "%WORK_DIR%\unpacked" md "%WORK_DIR%\unpacked"
+if not exist "%WORK_DIR%\modified" md "%WORK_DIR%\modified"
 if exist "%WORK_DIR%\%file%" %NODEJS% %mainjs% unpack "%WORK_DIR%\%file%" "%WORK_DIR%\unpacked"
-::for %%i in (unpacked*) do @move /Y %%i unpacked\
