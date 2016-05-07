f84431eb321bba628ff144f0c83d6c552646e893
myfreeer <myfreeer@gmail.com>
2016-05-07 10:59:57 +0800

    added a custom settings.ini for advanced users (will work without it)

--- replace.cmd
+++ replace.cmd
@@ -3,7 +3,10 @@ set NODEJS=node.exe
 set "WORK_DIR=%~dp0"
 set mainjs=node-chrome-pak.js
 set file=chrome_100_percent.pak
+set unpack-dir=unpacked
+set modified-dir=modified
 if not exist "%WORK_DIR%" md "%WORK_DIR%"
 pushd "%WORK_DIR%"
-if not exist "%WORK_DIR%\modified" md "%WORK_DIR%\modified" &exit
-if exist "%WORK_DIR%\%file%" for %%i in (modified\*) do %NODEJS% %mainjs% replace "%WORK_DIR%\%file%" %%~ni "%%~fsnxi"
+if exist settings.ini for /f "tokens=* eol=; delims=" %%i in (settings.ini) do set %%i
+if not exist "%WORK_DIR%\%modified-dir%" md "%WORK_DIR%\%modified-dir%" &exit
+if exist "%WORK_DIR%\%file%" for %%i in (%modified-dir%\*) do %NODEJS% %mainjs% replace "%WORK_DIR%\%file%" %%~ni "%%~fsnxi"
--- unpack.cmd
+++ unpack.cmd
@@ -3,8 +3,11 @@ set NODEJS=node.exe
 set "WORK_DIR=%~dp0"
 set mainjs=node-chrome-pak.js
 set file=chrome_100_percent.pak
+set unpack-dir=unpacked
+set modified-dir=modified
 if not exist "%WORK_DIR%" md "%WORK_DIR%"
 pushd "%WORK_DIR%"
-if not exist "%WORK_DIR%\unpacked" md "%WORK_DIR%\unpacked"
-if not exist "%WORK_DIR%\modified" md "%WORK_DIR%\modified"
-if exist "%WORK_DIR%\%file%" %NODEJS% %mainjs% unpack "%WORK_DIR%\%file%" "%WORK_DIR%\unpacked"
+if exist settings.ini for /f "tokens=* eol=; delims=" %%i in (settings.ini) do set %%i
+if not exist "%WORK_DIR%\%unpack-dir%" md "%WORK_DIR%\%unpack-dir%"
+if not exist "%WORK_DIR%\%modified-dir%" md "%WORK_DIR%\%modified-dir%"
+if exist "%WORK_DIR%\%file%" %NODEJS% %mainjs% unpack "%WORK_DIR%\%file%" "%WORK_DIR%\%unpack-dir%"
