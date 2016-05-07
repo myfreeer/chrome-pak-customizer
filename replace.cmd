c7ad4c09622da1d26dcf37be09a5f03d0f957932
myfreeer <myfreeer@gmail.com>
2016-05-07 10:42:04 +0800

    bug fix

--- replace.cmd
+++ replace.cmd
@@ -6,4 +6,4 @@ set file=chrome_100_percent.pak
 if not exist "%WORK_DIR%" md "%WORK_DIR%"
 pushd "%WORK_DIR%"
 if not exist "%WORK_DIR%\modified" md "%WORK_DIR%\modified" &exit
-if exist "%WORK_DIR%\%file%"for %i in (unpacked\*) do %NODEJS% %mainjs% replace "%WORK_DIR%\%file%" %~ni "%~fsnxi"
+if exist "%WORK_DIR%\%file%" for %%i in (modified\*) do %NODEJS% %mainjs% replace "%WORK_DIR%\%file%" %%~ni "%%~fsnxi"
