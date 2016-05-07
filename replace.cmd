52fd0eb240fbc6393f1788fa3ebce815cfe39ab1
myfreeer <myfreeer@gmail.com>
2016-05-07 20:02:40 +0800

    Leanify support added

--- replace.cmd
+++ replace.cmd
@@ -5,8 +5,12 @@ set mainjs=node-chrome-pak.js
 set file=chrome_100_percent.pak
 set unpack-dir=unpacked
 set modified-dir=modified
+set leanify=Leanify.exe
+set leanify_enabled=1
 if not exist "%WORK_DIR%" md "%WORK_DIR%"
 pushd "%WORK_DIR%"
 if exist settings.ini for /f "tokens=* eol=; delims=" %%i in (settings.ini) do set %%i
+if not exist %NODEJS% exit
 if not exist "%WORK_DIR%\%modified-dir%" md "%WORK_DIR%\%modified-dir%" &exit
+if %leanify_enabled%==1 if exist %leanify% %leanify% -q "%WORK_DIR%\%modified-dir%"
 if exist "%WORK_DIR%\%file%" for %%i in (%modified-dir%\*) do %NODEJS% %mainjs% replace "%WORK_DIR%\%file%" %%~ni "%%~fsnxi"
