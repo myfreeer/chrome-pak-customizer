d85ec43931d99e60f4201903b026a4622209d668
myfreeer <myfreeer@users.noreply.github.com>
2019-06-11 20:21:38 +0800

    file_type: better type detection
    * improved: html, js
    * added: svg, gz
    
    Added ~0.5KiB binary size

--- pak_get_file_type.c
+++ pak_get_file_type.c
@@ -3,11 +3,16 @@
 static const FileType FILE_TYPES[] = {
     PAK_GEN_FILE_TYPE(".png", "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"),
     PAK_GEN_FILE_TYPE(".html", "<!doctype html>"),
+    PAK_GEN_FILE_TYPE(".html", "<!DOCTYPE html>"),
     PAK_GEN_FILE_TYPE(".html", "<html>"),
+    PAK_GEN_FILE_TYPE(".html", "<!--"),
     PAK_GEN_FILE_TYPE(".html", "<link"),
+    PAK_GEN_FILE_TYPE(".svg", "<svg "),
     PAK_GEN_FILE_TYPE(".js", "// "),
+    PAK_GEN_FILE_TYPE(".js", "(function"),
     PAK_GEN_FILE_TYPE(".css", "/*"),
-    PAK_GEN_FILE_TYPE(".json", "{")};
+    PAK_GEN_FILE_TYPE(".json", "{"),
+    PAK_GEN_FILE_TYPE(".gz", "\x1f\x8b")};
 
 static const unsigned int FILE_TYPE_COUNT =
     sizeof(FILE_TYPES) / sizeof(FileType);
