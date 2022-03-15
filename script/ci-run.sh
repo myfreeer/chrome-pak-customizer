fc497c4c1b82324d896a60ba3271a640293a41fe
myfreeer <myfreeer@users.noreply.github.com>
2022-03-15 21:37:22 +0800

    ci: update script

--- script/ci-run.sh
+++ script/ci-run.sh
@@ -3,21 +3,25 @@
 rm -rf test_out
 mkdir -p test_out
 
+set -e
 is_success=1
 
 for file in ./test_dir/*.pak
 do
-  file_path="test_dir/${file}"
-  out_dir="test_out/${file}_out"
+  file_name=$(basename file)
+  file_path="test_dir/${file_name}"
+  out_dir="test_out/${file_name}_out"
   out_index="${out_dir}/pak_index.ini"
-  out_file="test_out/${file}_repack.pak"
+  out_file="test_out/${file_name}_repack.pak"
   cargo run --release u "$file_path" "$out_dir"
   cargo run --release p "$out_index" "$out_file"
   file_hash=$(sha256sum "$file_path" | cut -f1 -d ' ')
   out_hash=$(sha256sum "$out_file" | cut -f1 -d ' ')
-  if ! [ $file_hash = $out_hash ]; then
-      echo "file ${file} fail"
-      is_success=0
+  if ! [ "$file_hash" = "$out_hash" ]; then
+    echo "file ${file} fail"
+    is_success=0
+  else
+    echo "file ${file} passed"
   fi
 done
 
